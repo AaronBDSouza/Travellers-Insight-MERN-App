79f457af59d300d4ede205e0ac5a43e6daab57b6
AaronBDSouza <arrowknight007@gmail.com>
2021-11-29 23:58:49 +0530

    Frontend Routing

--- frontend/travellers-insight/src/App.js
+++ frontend/travellers-insight/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Users from './user/pages/Users';
+import Place from './places/pages/Place';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Main Page</h1>
-    </div>
-  );
+const App = () => {
+  return <Router>
+    <Routes>
+      <Route path="/user" element={<Users/>}/>
+    </Routes>
+    <Routes>
+      <Route path="/place" element={<Place/>}/>
+    </Routes>    
+    <Navigate to="/" />
+  </Router>;
 }
 
 export default App;
---
+++ frontend/travellers-insight/src/places/pages/Place.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Place = () => {
+    return <h2>This is the Place page</h2>;
+}
+
+export default Place;
