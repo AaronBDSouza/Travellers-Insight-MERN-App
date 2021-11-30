3ca17fefb342a290fdff3a89cb38b1beeb24038c
AaronBDSouza <arrowknight007@gmail.com>
2021-11-30 23:46:24 +0530

    Changed react-router-dom version
    Changed react-router-dom version from 6.0.2 to 5.0

--- frontend/travellers-insight/src/App.js
+++ frontend/travellers-insight/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import Place from './places/pages/Place';
 
 const App = () => {
+  /* For react-router-dom 6.0.2
+  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
   return <Router>
     <Routes>
+      <Route path="/user" element={<App/>}/>
       <Route path="/user" element={<Users/>}/>
-    </Routes>
-    <Routes>
       <Route path="/place" element={<Place/>}/>
-    </Routes>    
-    <Navigate to="/" />
-  </Router>;
+      <Route path="*" element={<App/>}/>
+    </Routes>
+  </Router>;*/
+
+    return <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users/>
+        </Route>
+        <Route path="/places/new" exact>
+          <Place/>
+        </Route>  
+        <Redirect to="/" />      
+      </Switch>
+    </Router>
+  
+
 }
 
 export default App;
