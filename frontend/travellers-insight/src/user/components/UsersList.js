28ccd6bbf7ec0aaecd05ae3d86f8fcabd465c993
AaronBDSouza <arrowknight007@gmail.com>
2021-11-30 23:58:44 +0530

    Added UsersList and UserItem Components

---
+++ frontend/travellers-insight/src/user/components/UsersList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './UsersList.css';
+import UserItem from './UserItem';
+
+const UsersList = props => {
+    if(props.items.length = 0){
+        return (<div className="center">
+            <h2>No users found.</h2>
+        </div>
+        );
+    }
+
+    return <ul>
+        {props.items.map(user => (
+        <UserItem key={user.id}/>
+        ))}
+    </ul>;
+};
+
+export default UsersList;
