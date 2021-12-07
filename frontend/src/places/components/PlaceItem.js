038205678c5285bda3b8580fe2bfa6830a501600
AaronBDSouza <arrowknight007@gmail.com>
2021-12-07 23:59:25 +0530

    Integrated Google Map Functionality

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -2,6 +2,7 @@ import React,{ useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
 
 import './PlaceItem.css';
 
@@ -19,7 +20,12 @@ const PlaceItem = props => {
             footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
             >
                 <div className="map-container">
-                    <h2>THE MAP!</h2>
+                    {/*
+                        <h2>THE MAP!</h2>
+                        <Map center={props.coordinates} zoom={16}/>
+                    */}
+
+                    <iframe title="myFrame" className="map-iframe-container" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12090.470159428245!2d-73.9856644!3d40.7484405!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sin!4v1638897516220!5m2!1sen!2sin" allowfullscreen="" loading="lazy" ></iframe>
                 </div>
             </Modal>
             <li className="place-item">
