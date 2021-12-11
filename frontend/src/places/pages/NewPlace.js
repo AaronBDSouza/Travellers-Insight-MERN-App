69a77fe9aa206733ad394638850353493a7e5993
AaronBDSouza <arrowknight007@gmail.com>
2021-12-11 23:58:22 +0530

    Added Add Place Functionality

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+
 const NewPlace = () => {
-    return <h2>This is the Place page</h2>;
+    return <form className="place-form">
+        <label></label>
+        {/*<Input type="text" label="Title" validators={[]} onChange={}/>*/}
+        <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title"/>
+    </form>;
 }
 
 export default NewPlace;
