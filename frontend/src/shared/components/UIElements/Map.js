8583c6597aa60518be1679d6434dddab1bb55bb5
AaronBDSouza <arrowknight007@gmail.com>
2021-12-11 23:00:57 +0530

    Fixed Google Maps Styling Issue

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -4,22 +4,22 @@ import './Map.css';
 const Map = props => {
     const mapRef = useRef();
 
+    //Object Destructuring instead of using props.center or props.zoom
     const { center, zoom } = props;
 
     useEffect(() => {
         const map = new window.google.maps.Map(mapRef.current, {
             center: center,
-            zoom: zoom,
+            zoom: zoom
         });
 
-        new window.google.maps.Marker({position: props.center, map:map })
+        new window.google.maps.Marker({position: center, map:map })
     }, [center, zoom]);
 
     
 
     return (
         <div ref ={mapRef} className={`map ${props.className}`} style={props.style}>
-            
         </div>
     );
 }
