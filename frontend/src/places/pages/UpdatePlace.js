a3037bde71cc54fffbc939578542e22f17f1b1d2
AaronBDSouza <arrowknight007@gmail.com>
2021-12-16 19:16:04 +0530

    Updated Custom Hook

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
@@ -35,20 +35,46 @@ const DUMMY_PLACES = [
 
 const UpdatePlace = () => {
 
+    const [ isLoading, setIsLoading ] = useState(true);
+
     const placeId = useParams().placeId;
+
+    const [ formState, inputHandler, setFormData ] = useForm(
+        {
+            title:{
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
+        }, 
+        false
+    );
 
     const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
-    const [ formState, inputHandler ] = useForm({
-        title:{
-            value: identifiedPlace.title,
-            isValid: true
-        },
-        description: {
-            value: identifiedPlace.description,
-            isValid: true
-        }
-    }, true)
+    useEffect(() => {
+        setFormData({
+            title:{
+                value: identifiedPlace.title,
+                isValid: true
+            },
+            description: {
+                value: identifiedPlace.description,
+                isValid: true
+            }
+        }, true);
+        setIsLoading(false);
+    }, [ setFormData, identifiedPlace]);
+
+
+
+    const placeUpdateSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    };
 
     if (!identifiedPlace) {
         return (
@@ -58,7 +84,16 @@ const UpdatePlace = () => {
         )
     }
 
-    return <form className='place-form'>
+    if (isLoading) {
+        return (
+            <div className='center'>
+                <h2>Loading...</h2>
+            </div>
+        );
+    }    
+
+    return (
+    <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
         <Input 
             id="title" 
             element="input" 
@@ -80,10 +115,11 @@ const UpdatePlace = () => {
             initialValue={formState.inputs.description.value}
             initialValid={formState.inputs.description.isValid}
         />
-        <Button type="submit" disabled={true}>
+        <Button type="submit" disabled={!formState.isValid}>
             UPDATE PLACE
         </Button>        
-    </form>;
-}
+    </form>
+    );
+};
 
 export default UpdatePlace;
