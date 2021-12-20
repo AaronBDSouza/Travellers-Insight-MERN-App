77bb39e21c4ebe15fd75ab58fefe8861f48b341b
AaronBDSouza <arrowknight007@gmail.com>
2021-12-20 23:35:25 +0530

    Updated Auth Component with Sign Up functionality

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import './Auth.css';
 
 const Auth = () =>  {
-    const [ formState, inputHandler ] =  useForm({
+    const [ isLoginMode, setIsLoginMode ] = useState(true);
+
+    const [ formState, inputHandler, setFormData ] =  useForm({
         email: {
           value: '',
           isValid: false  
@@ -18,6 +20,31 @@ const Auth = () =>  {
         }
     });
 
+    const swithModeHandler = () => {
+        if (!isLoginMode) {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: undefined
+                }, 
+                formState.inputs.email.isValid && formState.inputs.password.isValid
+            );
+        }
+        else {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: {
+                        value: '',
+                        isValid: false
+                    }
+                }, 
+                false
+            );
+        }
+
+        setIsLoginMode(prevMode => !prevMode);
+    };
 
     const authSubmitHandler = event => {
         event.preventDefault();
@@ -31,6 +58,17 @@ const Auth = () =>  {
             <hr/>
             <form onSubmit={authSubmitHandler}>
                 <label></label>
+                {!isLoginMode && 
+                <Input
+                    id="name"
+                    element="input"
+                    type="text"
+                    label="Your Name"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a name."
+                    onInput={inputHandler}
+                />}
+
                 <Input 
                     id="email"
                     element="input" 
@@ -40,6 +78,7 @@ const Auth = () =>  {
                     errorText="Please enter a valid email address"
                     onInput={inputHandler}
                 />        
+
                 <Input 
                     id="password"
                     element="input" 
@@ -50,9 +89,12 @@ const Auth = () =>  {
                     onInput={inputHandler}
                 />
                 <Button type="submit" disabled={!formState.isValid}>
-                   LOGIN
+                   {isLoginMode ? 'LOGIN' : 'SIGNUP' }
                 </Button>
             </form>
+            <Button inverse onClick={swithModeHandler}>
+                SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+            </Button>
     </Card>
     );
 }
